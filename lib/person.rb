7e4bcc43966251a5a004bc4e02d590bf7a67a063
solomonfy <fantahunsolomon@gmail.com>
2020-07-08 17:49:17 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -46,22 +46,25 @@ class Person
     def call_friend(friend)
         self.happiness = @happiness + 3
         friend.happiness = friend.happiness + 3
-        return "Hi #{friend.name}! It's #{@name}. How are you?"
+        return "Hi #{friend.name}! It's #{self.name}. How are you?"
     end
 
+    
     def start_conversation(friend, topic)
-        if topic == "politics"
-          friend.happiness -=2
-          self.happiness -=2
-          return "blah blah partisan blah lobbyist"
-        elsif topic == "weather"
-          friend.happiness +=1
-          self.happiness +=1
-          return "blah blah sun blah rain"
-        else
-          return "blah blah blah blah blah"
-        end
+        case topic
+            when "politics"
+                friend.happiness -=2
+                self.happiness -=2
+                return "blah blah partisan blah lobbyist"
+            when "weather"
+                friend.happiness +=1
+                self.happiness +=1
+                return "blah blah sun blah rain"
+            else
+                return "blah blah blah blah blah"
+            end
     end
+
 
 end
 
